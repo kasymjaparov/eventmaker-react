b5d5adfcf8abb74e39e9b5f6e5d5e7337278360a
kasymjaparov <kasymjaparov@gmail.com>
2022-01-04 12:34:42 +0600

    style bug fix

--- src/App.js
+++ src/App.js
@@ -1,43 +1,63 @@
-import { BrowserRouter, Redirect, Switch } from 'react-router-dom'
-import CommonRoute from './routes/CommonRoute.jsx'
-import PrivateRoute from './routes/PrivateRoute.jsx'
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { getUserInfo } from './store/actions/auth.js'
-import { getAllEvents } from './store/actions/events.js'
-import Loading from './components/Loading/Loading.js'
-import { getTypes } from './store/actions/events'
-import { getAllUsers } from './store/actions/auth'
+import React from "react"
+import { BrowserRouter, Switch } from "react-router-dom"
+import Loading from "./components/Loading/Loading.js"
+import CommonRoute from "./routes/CommonRoute.jsx"
+import PrivateRoute from "./routes/PrivateRoute.jsx"
+import { useDispatch, useSelector } from "react-redux"
+import { getUserInfo } from "./store/actions/auth.js"
+import { getAllEvents } from "./store/actions/events.js"
+import { getTypes } from "./store/actions/events"
+import { getAllUsers } from "./store/actions/auth"
 
 function App() {
-	const dispatch = useDispatch()
-	const token = useSelector((state) => state.auth.googleLogin.token)
-	const success = useSelector((state) => state.auth.get.success)
-	const loading = useSelector((state) => state.auth.get.loading)
-	const loginSuccess = useSelector((s) => s.auth.login.success)
-	const typesLoading = useSelector((s) => s.events.getAllTypes.loading)
-	const eventsLoading = useSelector(s=>s.events.getAllEvents.loading)
-	const eventLoading = useSelector((s) => s.events.eventState.loading)
-	const getUserStateLoading = useSelector((s) => s.events.getUserState.loading)
+  const dispatch = useDispatch()
+  const token = useSelector(state => state.auth.googleLogin.token)
+  const success = useSelector(state => state.auth.get.success)
+  const loading = useSelector(state => state.auth.get.loading)
+  const loginSuccess = useSelector(s => s.auth.login.success)
+  const typesLoading = useSelector(s => s.events.getAllTypes.loading)
+  const eventsLoading = useSelector(s => s.events.getAllEvents.loading)
+  const eventLoading = useSelector(s => s.events.eventState.loading)
+  const getUserStateLoading = useSelector(s => s.events.getUserState.loading)
 
-	React.useEffect(() => {
-		if (window.localStorage.getItem('token') || window.localStorage.getItem('google') || token || loginSuccess) {
-			dispatch(getUserInfo(window.localStorage.getItem('token') || window.localStorage.getItem('google')))
-		}
-	}, [window.localStorage.getItem('token'), window.localStorage.getItem('google'), token, loginSuccess])
+  React.useEffect(() => {
+    if (
+      window.localStorage.getItem("token") ||
+      window.localStorage.getItem("google") ||
+      token ||
+      loginSuccess
+    ) {
+      dispatch(
+        getUserInfo(
+          window.localStorage.getItem("token") ||
+            window.localStorage.getItem("google")
+        )
+      )
+    }
+  }, [
+    window.localStorage.getItem("token"),
+    window.localStorage.getItem("google"),
+    token,
+    loginSuccess,
+  ])
 
-	React.useEffect(() => {
-		dispatch(getAllEvents())
-		dispatch(getTypes())
-		dispatch(getAllUsers())
-	}, [])
-
-	return (
-		<BrowserRouter>
-			{(getUserStateLoading || loading || typesLoading || eventLoading || eventsLoading) && <Loading />}
-			<Switch>{success ? <PrivateRoute /> : <CommonRoute />}</Switch>
-		</BrowserRouter>
-	)
+  React.useEffect(() => {
+    dispatch(getAllEvents())
+    dispatch(getTypes())
+    dispatch(getAllUsers())
+  }, [])
+  const showLoader =
+    getUserStateLoading ||
+    loading ||
+    typesLoading ||
+    eventLoading ||
+    eventsLoading
+  return (
+    <BrowserRouter>
+      {showLoader && <Loading />}
+      <Switch>{success ? <PrivateRoute /> : <CommonRoute />}</Switch>
+    </BrowserRouter>
+  )
 }
 
 export default App
--- src/components/UserEvents/UserEvents.js
+++ src/components/UserEvents/UserEvents.js
@@ -1,69 +1,81 @@
-import React, {useEffect, useState} from 'react'
-import {getUserEvents} from '../../store/actions/events'
-import {useDispatch, useSelector} from 'react-redux'
-import './UserEvents.css'
-import Container from '../Container'
-import UserEventsList from './UserEventsList/UserEventsList'
-import UserEventsPagination from './UserEventsPagination/UserEventsPagination'
-import {useParams} from 'react-router'
+import React, { useEffect, useState } from "react"
+import { getUserEvents } from "../../store/actions/events"
+import { useDispatch, useSelector } from "react-redux"
+import "./UserEvents.css"
+import Container from "../Container"
+import UserEventsList from "./UserEventsList/UserEventsList"
+import UserEventsPagination from "./UserEventsPagination/UserEventsPagination"
+import Loading from "../Loading/Loading.js"
+import { useParams } from "react-router"
 
 export default function UserEvents() {
-    const dispatch = useDispatch()
-    const {page} = useParams()
-    const showPerPage = 6
-    const [pagination, setPagination] = useState({
-        start: showPerPage * (+page - 1),
-        end: showPerPage * +page
-    })
-    const [events, setEvents] = useState([])
-    const userEvents = useSelector((state) => state.events.userEvents)
-    const searchValue = useSelector((s) => s.events.searchEvent)
-    const onPaginationChange = (start, end) => {
-        setPagination({start: start, end: end})
+  const dispatch = useDispatch()
+  const { page } = useParams()
+  const showPerPage = 6
+  const [pagination, setPagination] = useState({
+    start: showPerPage * (+page - 1),
+    end: showPerPage * +page,
+  })
+  const [events, setEvents] = useState([])
+  const userEvents = useSelector(state => state.events.userEvents)
+  const searchValue = useSelector(s => s.events.searchEvent)
+  const onPaginationChange = (start, end) => {
+    setPagination({ start: start, end: end })
+  }
+  React.useEffect(() => {
+    dispatch(getUserEvents())
+  }, [])
+  const deleteEventDisplay = useSelector(s => s.events.deleteEventDisplay)
+  const loading = useSelector(s => s.events.userEventState.loading)
+  useEffect(() => {
+    if (deleteEventDisplay === "block") {
+      document.querySelector("body").style.overflow = "hidden"
+    } else {
+      document.querySelector("body").style.overflow = "auto"
     }
-    React.useEffect(() => {
-        dispatch(getUserEvents())
-    }, [])
-    const deleteEventDisplay = useSelector((s) => s.events.deleteEventDisplay)
-    useEffect(() => {
-        if (deleteEventDisplay === 'block') {
-            document.querySelector('body').style.overflow = 'hidden'
-        } else {
-            document.querySelector('body').style.overflow = 'auto'
-        }
-    }, [deleteEventDisplay])
-    useEffect(() => {
-        setEvents(userEvents)
-        if (searchValue) {
-            setEvents(userEvents.filter((event) => event.title.toLowerCase().includes(searchValue.toLowerCase())))
-        }
-    }, [userEvents, searchValue])
+  }, [deleteEventDisplay])
+  useEffect(() => {
+    setEvents(userEvents)
+    if (searchValue) {
+      setEvents(
+        userEvents.filter(event =>
+          event.title.toLowerCase().includes(searchValue.toLowerCase())
+        )
+      )
+    }
+  }, [userEvents, searchValue])
 
-    return (
-        <div
-            className={'userEvents'}
-            style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between', minHeight: '100vh'}}
-        >
-            <Container>
-                {events.length === 0 ? (
-                    <div className='main_title'>Нет событий</div>
-                ) : (
-                    <div className='main_title'>Список моих событий </div>
-                )}
-                <div className='main_cards' style={{marginBottom: '40px'}}>
-                    {events.slice(pagination.start, pagination.end).map((item) => (
-                        <UserEventsList data={item} key={item._id}/>
-                    ))}
-                </div>
-                {events.length !== 0 && (
-                    <UserEventsPagination
-                        page={page}
-                        showPerPage={showPerPage}
-                        onPaginationChange={onPaginationChange}
-                        total={events.length}
-                    />
-                )}
-            </Container>
+  return (
+    <div
+      className={"userEvents"}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        minHeight: "100vh",
+      }}
+    >
+      <Container>
+        {!loading && events.length === 0 ? (
+          <div className='main_title'>Нет событий</div>
+        ) : (
+          <div className='main_title'>Список моих событий </div>
+        )}
+        {loading && events.length === 0 && <div>Загрузка...</div>}
+        <div className='main_cards' style={{ marginBottom: "40px" }}>
+          {events.slice(pagination.start, pagination.end).map(item => (
+            <UserEventsList data={item} key={item._id} />
+          ))}
         </div>
-    )
+        {events.length !== 0 && (
+          <UserEventsPagination
+            page={page}
+            showPerPage={showPerPage}
+            onPaginationChange={onPaginationChange}
+            total={events.length}
+          />
+        )}
+      </Container>
+    </div>
+  )
 }
